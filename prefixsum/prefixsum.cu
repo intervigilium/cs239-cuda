4ef437d0da31bd97da2e356eae0ae744de1edd9f
Ethan Chen <intervigil@gmail.com>
2011-05-19 03:20:46 -0700

    initial implementation of prefixsum

--- prefixsum/prefixsum.cu
+++ prefixsum/prefixsum.cu
@@ -8,48 +8,99 @@
 #include <string.h>
 #include <unistd.h>
 
+#define NUM_BANKS 16
+#define LOG_NUM_BANKS 4
+#define CONFLICT_FREE_OFFSET(n) ((n) >> NUM_BANKS + (n) >> (2 * LOG_NUM_BANKS))
+
 #define NUM_BLOCKS 32
 #define BLOCK_SIZE 512
 
-__global__ void reduce(int *in_array, int *out_array, int size)
+__global__ void cuda_prefixsum(int *in_array, int *out_array, int size)
 {
-	// gridDim.x * blockDim.x >= size / 2
 	// shared should be sized to blockDim.x
+	extern __shared__ int shared[];
+
+	unsigned int tid = threadIdx.x;
+	unsigned int offset = 1;
+
+	int i = tid;
+	int j = tid + size / 2;
+	int offset_i = CONFLICT_FREE_OFFSET(i);
+	int offset_j = CONFLICT_FREE_OFFSET(j);
+	shared[i + offset_i] = in_array[i];
+	shared[j + offset_j] = in_array[j];
+
+	// scan up
+	for (int s = size >> 1; s > 0; s >>= 1) {
+		__syncthreads();
+
+		if (tid < s) {
+			int i = offset * (2 * tid + 1) - 1;
+			int j = offset * (2 * tid + 2) - 1;
+			i += CONFLICT_FREE_OFFSET(i);
+			j += CONFLICT_FREE_OFFSET(j);
+			shared[j] += shared[i];
+		}
+		offset <<= 1;
+	}
+
+	if (tid == 0) {
+		shared[size - 1 + CONFLICT_FREE_OFFSET(size - 1)] = 0;
+	}
+	// scan down
+	for (int s = 1; s < size; s <<= 1) {
+		offset >>= 1;
+		__syncthreads();
+
+		if (tid < s) {
+			int i = offset * (2 * tid + 1) - 1;
+			int j = offset * (2 * tid + 2) - 1;
+			i += CONFLICT_FREE_OFFSET(i);
+			j += CONFLICT_FREE_OFFSET(j);
+			int tmp = shared[i];
+			shared[i] = shared[j];
+			shared[j] += tmp;
+		}
+	}
+	__syncthreads();
+	// exclusive scan, need to shift all elements left, sum last one
+
+	// copy data back to main memory
+	// scan is exclusive, make it inclusive by left shifting elements
+	if (tid > 0) {
+		in_array[i] = shared[i + offset_i];
+	} else {
+		// re-calc the last element, drop it in out array
+		in_array[size - 1] +=
+		    shared[size - 1 + CONFLICT_FREE_OFFSET(size - 1)];
+		out_array[blockIdx.x] = in_array[size - 1];
+	}
+	in_array[j] = shared[offset_j];
+}
+
+__global__ void cuda_updatesum(int *array, int *update_array, int size)
+{
 	extern __shared__ int shared[];
 
 	unsigned int tid = threadIdx.x;
 	unsigned int id = blockIdx.x * (blockDim.x * 2) + threadIdx.x;
 
-	// first sum
-	int t_sum = (id < size) ? in_array[id] : 0;
+	int op = update_array[blockIdx.x];
+
+	if (id < size) {
+		shared[tid] = array[id] + op;
+		array[id + blockDim.x] = shared[tid + blockDim.x];
+	}
 	if (id + blockDim.x < size) {
-		// current reduce function is sum
-		t_sum = sum(in_array[id + blockDim.x], t_sum);
+		shared[tid + blockDim.x] = array[id + blockDim.x] + op;
+		array[id + blockDim.x] = shared[tid + blockDim.x];
 	}
 
-	shared[tid] = t_sum;
-	__syncthreads();
-
-	// do reduction in shared mem
-	for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
-		if (tid < s) {
-			// current reduce function is sum
-			t_sum = sum(t_sum, shared[tid + s]);
-			shared[tid] = t_sum;
-		}
-		__syncthreads();
-	}
-
-	if (tid == 0) {
-		out_array[blockIdx.x] = shared[0];
-	}
 }
 
 void prefixsum(int blocks, int threads, int *array_h, int size)
 {
-	int res;
 	int *array_d;
-	int *in_ptr_d;
 	int *out_array_d;
 	int *tmp_d;
 
@@ -59,43 +110,30 @@ void prefixsum(int blocks, int threads, int *array_h, int size)
 	cudaMalloc((void **)&array_d, size * sizeof(int));
 	cudaMemcpy(array_d, array_h, size * sizeof(int),
 		   cudaMemcpyHostToDevice);
-	in_ptr_d = array_d;
 
-	// do map
-	map <<< dim_grid, dim_block, threads * sizeof(int) >>> (array_d, size);
+	cudaMalloc((void **)&out_array_d, blocks * sizeof(int));
+	cudaMalloc((void **)&tmp_d, blocks * sizeof(int));
+	cuda_prefixsum <<< dim_grid, dim_block,
+	    threads * sizeof(int) >>> (array_d, out_array_d, size);
+	cuda_prefixsum <<< dim_grid, dim_block,
+	    threads * sizeof(int) >>> (out_array_d, tmp_d, blocks);
+	cuda_updatesum <<< dim_grid, dim_block,
+	    threads * sizeof(int) >>> (array_d, out_array_d, size);
 
-	// do reduce in iterations
-	for (unsigned int s = blocks / 2; s > 0; s >>= 1) {
-		// allocate array equal to blocks/2
-		cudaMalloc((void **)&out_array_d, s * sizeof(int));
-		reduce <<< dim_grid, dim_block,
-		    threads * sizeof(int) >>> (in_ptr_d, out_array_d, size);
-
-		// free up the old input array
-		tmp_d = in_ptr_d;
-		// should check for tmp_d != array_h but we can just fail silently
-		cudaFree(tmp_d);
-
-		// set input array of next iteration to output array
-		in_ptr_d = out_array_d;
-		size = s;
-	}
-
-	// retrieve result
-	cudaMemcpy(&res, out_array_d, sizeof(int), cudaMemcpyDeviceToHost);
+	// copy resulting array back to host
+	cudaMemcpy(array_h, array_d, size * sizeof(int),
+		   cudaMemcpyDeviceToHost);
 
 	cudaFree(out_array_d);
 	cudaFree(array_d);
-
-	return res;
 }
 
 void prefixsum_host(int *array_h, int size)
 {
 	for (int i = 0; i < size; i++) {
-        if (i > 0) {
-		    array_h[i] += array_h[i - 1];
-        }
+		if (i > 0) {
+			array_h[i] += array_h[i - 1];
+		}
 	}
 }
 
