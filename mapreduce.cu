77542aa4ace1cfb801a13a593cd3bb1417086872
Ethan Chen <intervigil@gmail.com>
2011-05-19 00:23:28 -0700

    use mapreduce driver function

--- mapreduce.cu
+++ mapreduce.cu
@@ -90,6 +90,51 @@ __global__ void reduce(int *in_array, int *out_array, int size)
 	}
 }
 
+int mapreduce(int blocks, int threads, int *array_h, int size)
+{
+	int res;
+	int *array_d;
+	int *in_ptr_d;
+	int *out_array_d;
+	int *tmp_d;
+
+	dim3 dim_grid(blocks, 1, 1);
+	dim3 dim_block(threads, 1, 1);
+
+	cudaMalloc((void **)&array_d, size * sizeof(int));
+	cudaMemcpy(array_d, array_h, size * sizeof(int),
+		   cudaMemcpyHostToDevice);
+	in_ptr_d = array_d;
+
+	// do map
+	map <<< dim_grid, dim_block, threads * sizeof(int) >>> (array_d, size);
+
+	// do reduce in iterations
+	for (unsigned int s = blocks / 2; s > 0; s >>= 1) {
+		// allocate array equal to blocks/2
+		cudaMalloc((void **)&out_array_d, s * sizeof(int));
+		reduce <<< dim_grid, dim_block,
+		    threads * sizeof(int) >>> (in_ptr_d, out_array_d, size);
+
+		// free up the old input array
+		tmp_d = in_ptr_d;
+		// should check for tmp_d != array_h but we can just fail silently
+		cudaFree(tmp_d);
+
+		// set input array of next iteration to output array
+		in_ptr_d = out_array_d;
+		size = s;
+	}
+
+	// retrieve result
+	cudaMemcpy(&res, out_array_d, sizeof(int), cudaMemcpyDeviceToHost);
+
+	cudaFree(out_array_d);
+	cudaFree(array_d);
+
+	return res;
+}
+
 void usage(int which)
 {
 	switch (which) {
